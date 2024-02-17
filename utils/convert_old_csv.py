0b143d6b75c8c2f92442beac21eb5b4d0b584d8f
Guanghui Qin <hiaoxui@gmail.com>
2024-02-17 14:18:59 -0500

    convert with C names

--- utils/convert_old_csv.py
+++ utils/convert_old_csv.py
@@ -17,13 +17,18 @@ def run():
     for f in os.listdir(args.i):
         if not f.endswith('.csv'):
             continue
-        df = pd.read_csv(os.path.join(args.i, f))
+        df = pd.read_csv(os.path.join(args.i, f), encoding='iso-8859-1')
         tgt_folder = os.path.join(args.o, f'cell_{cell_order:03}')
         os.makedirs(tgt_folder, exist_ok=True)
-        shutil.copy(os.path.join(args.i, f), os.path.join(tgt_folder, f'cell_{cell_order:03}_measurements.csv'))
-        shutil.copy(os.path.join(args.i, f[:-4]+'.zip'), os.path.join(tgt_folder, 'RoiSet.zip'))
+        src_zip_path = os.path.join(args.i, f[:-4]+'.zip')
+        tgt_csv_path = os.path.join(tgt_folder, f'cell_{cell_order:03}_measurements.csv')
+        if os.path.exists(src_zip_path):
+            shutil.copy(src_zip_path, os.path.join(tgt_folder, 'RoiSet.zip'))
+        # hard-coded for chemotaxis; sometimes it is C3 and C4 for X and Y
+        df = df.rename(columns={'C3': 'X', 'C4': 'Y'})
+        df.to_csv(tgt_csv_path)
         meta = {
-            'x': df.iloc[0].X, 'y': df.iloc[0].Y, 'cell_name': f'cell_{cell_order:03}',
+            'x': df.iloc[0].X, 'y': df.iloc[0].X, 'cell_name': f'cell_{cell_order:03}',
             'timestamp': os.path.getmtime(os.path.join(args.i, f)),
             'pixel_size': args.pixel_size, 'time_deltas': [args.time_interval] * len(df),
             'pixel_size_unit': 'µm', 'czi_path': f'cli://{args.pixel_size}:{args.time_interval}'
